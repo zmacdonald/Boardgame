f5b3ec43cdf05bc9384d638a8216721778a65be3
bdnk <brendanjkeyworth@gmail.com>
2014-10-28 09:31:22 -0400

    Initial Commit

---
+++ Boardgame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Boardgame
+//
+//  Created by Brendan Keyworth on 2014-10-28.
+//  Copyright (c) 2014 BrendanKeyworth. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
